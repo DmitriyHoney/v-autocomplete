c7df14c06d8aa83f0e585e227cd5d7912f2fed4f
Dmitry Bolshakov <eur719@gmail.com>
2020-12-17 17:34:57 +0300

    first

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
+    <AutocompleteField />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import AutocompleteField from '@/components/AutocompleteField'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { AutocompleteField }
 }
 </script>
